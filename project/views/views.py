a8772edb16ca850b472c00e4e3c9a65f753d982c
Adam Lieskovsky <adamliesko@gmail.com>
2013-07-31 09:57:28 +0200

    deleting question now changes numbering of others

--- project/views/views.py
+++ project/views/views.py
@@ -55,7 +55,7 @@ import json
 
 from sqlalchemy import (
     asc,
-    desc,
+    desc
     )
 
 from sqlalchemy.sql import func
@@ -459,12 +459,13 @@ def question_delete(request):
     questionid = request.matchdict['question_id']
     question= request.db_session.query(Question).filter_by(id=questionid).one()
     question_number=question.number
-    questions_with_number_to_be_changed = request.db_session.query(Question).filter(Question.number >question.number).all()
+    questions_with_number_to_be_changed = request.db_session.query(Question).filter(Question.number > question_number).all()
     for q in questions_with_number_to_be_changed:
         q.number=q.number-1
-        request.db_session.save(q);
-    request.db_session.delete(question)
 
+
+    request.db_session.delete(question)
+    request.db_session.flush()
     return HTTPFound(request.route_path('showtest', test_id=testid))
 
 
